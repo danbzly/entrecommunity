2faf381c7dcb410288b49238c45028f4cbeeceff
Daniel Beasley <DanBzly@gmail.com>
2013-03-01 12:40:15 -0500

    testing Profile name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -36,14 +36,20 @@ test "a user should have a unique profile name" do
  end
  
 test "a user should have a profile name without spaces" do
-	user = User.new
+	user = User.new(first_name: 'daniel', last_name: 'beasley', email: 'danbzly@gmail.com')
+	user.password = user.password_confirmation = 'asdfasdf'
+
 	user.profile_name = "My profile With Spaces"
 
 	assert	!user.save
 	assert	!user.errors[:profile_name].empty?
 	assert	user.errors[:profile_name].include?("Must be formatted correctly.")
 
+test "a user can have a correctly formatted profile name " do
+	user = User.new(first_name: 'daniel', last_name: 'beasley', email: 'danbzly@gmail.com')
+	user.password = user.password_confirmation = 'asdfasdf'
 
-
+	user.profile_name = 'daniel'
+	assert user.valid?
  end
 end
