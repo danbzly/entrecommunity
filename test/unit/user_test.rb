2fc5f1fef12597636f4f5ec9e42ed79311ed39f2
Daniel Beasley <DanBzly@gmail.com>
2013-03-01 17:41:30 -0500

    test for frienships

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
+
   
 test "a user should enter a first name" do
 	user = User.new
@@ -44,12 +48,25 @@ test "a user should have a profile name without spaces" do
 	assert	!user.save
 	assert	!user.errors[:profile_name].empty?
 	assert	user.errors[:profile_name].include?("Must be formatted correctly.")
-
+ end
 test "a user can have a correctly formatted profile name " do
 	user = User.new(first_name: 'daniel', last_name: 'beasley', email: 'danbzly@gmail.com')
 	user.password = user.password_confirmation = 'asdfasdf'
 
-	user.profile_name = 'daniel'
-	assert user.valid?
+	user.profile_name = 'daniel_1'
+	
  end
+
+test"that no error is raised when trying to access a friend list" do
+assert_nothing_raised do
+  users(:daniel).friends
+  end
+ end
+
+test"that creating friendships on a user works" do 
+	users(:daniel).friends << users(:mike)
+	users(:daniel).friends.reload
+	assert users(:daniel).friends.include?(users(:mike))
+end
+
 end
